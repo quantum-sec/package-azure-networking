0da20ae592ff58de3865a24f6261d98c979efa06
Luke Fritz <lukefritz@gmail.com>
2020-06-24 16:51:31 -0500

    Add descriptions to virtual network outputs (patch)

--- modules/azure-virtual-network/outputs.tf
+++ modules/azure-virtual-network/outputs.tf
@@ -1,19 +1,19 @@
 output "id" {
-  value = azurerm_virtual_network.network.id
+  description = "The ID of the virtual network."
+  value       = azurerm_virtual_network.network.id
 }
 
 output "name" {
-  value = azurerm_virtual_network.network.name
+  description = "The name of the virtual network."
+  value       = azurerm_virtual_network.network.name
 }
 
 output "address_space" {
-  value = azurerm_virtual_network.network.address_space
+  description = "The IP address space of the virtual network."
+  value       = azurerm_virtual_network.network.address_space
 }
 
 output "guid" {
-  value = azurerm_virtual_network.network.guid
-}
-
-output "subnets" {
-  value = azurerm_virtual_network.network.subnet
+  description = "The GUID of the virtual network."
+  value       = azurerm_virtual_network.network.guid
 }
