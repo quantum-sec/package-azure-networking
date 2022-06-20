ea1f1cfc1c6a927f2258f8f068903789a3ac0417
Chris Jaimon <chris.jaimon@quantum.security>
2022-06-20 14:21:06 +0530

    XDR-3497: update azure-network-security-rule configuration - add `source_port_range` - add `source_address_prefix` - add `destination_port_range` - add `destination_address_prefix`

--- modules/azure-network-security-rule/main.tf
+++ modules/azure-network-security-rule/main.tf
@@ -16,11 +16,15 @@ resource "azurerm_network_security_rule" "rule" {
   priority                    = var.priority
   direction                   = var.direction
 
+  source_port_range                     = var.source_port_range
   source_port_ranges                    = var.source_port_ranges
+  source_address_prefix                 = var.source_address_prefix
   source_address_prefixes               = var.source_address_prefixes
   source_application_security_group_ids = var.source_application_security_group_ids
 
+  destination_port_range                     = var.destination_port_range
   destination_port_ranges                    = var.destination_port_ranges
+  destination_address_prefix                 = var.destination_address_prefix
   destination_address_prefixes               = var.destination_address_prefixes
   destination_application_security_group_ids = var.destination_application_security_group_ids
 }
